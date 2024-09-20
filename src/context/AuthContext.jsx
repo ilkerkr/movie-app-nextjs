1827999072f145613cea04cbb99edaa5f7a5a737
İlker <ilker.kircicek11@gmail.com>
2024-09-20 15:49:58 +0300

    project

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,127 @@
+"use client";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "@/auth/firebase";
+import {
+  toastErrorNotify,
+  toastSuccessNotify,
+  toastWarnNotify,
+} from "@/helpers/ToastNotify";
+import { useRouter } from "next/navigation";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    userObserver();
+  }, []);
+
+  const createUser = async (email, password, displayName) => {
+    try {
+      let userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      await updateProfile(auth.currentUser, {
+        displayName: displayName,
+      });
+      router.push("/login");
+      toastSuccessNotify("Registered successfully!");
+      console.log(userCredential);
+    } catch (err) {
+      toastErrorNotify(err.message);
+    }
+  };
+
+  const signIn = async (email, password) => {
+    try {
+      let userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      router.push("/profile");
+      toastSuccessNotify("Logged in successfully!");
+      console.log(userCredential);
+    } catch (err) {
+      toastErrorNotify(err.message);
+    }
+  };
+
+  const logOut = () => {
+    signOut(auth);
+    toastSuccessNotify("Logged out successfully!");
+  };
+
+  const userObserver = () => {
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        const { email, displayName, photoURL } = currentUser;
+        setCurrentUser({ email, displayName, photoURL });
+        sessionStorage.setItem(
+          "user",
+          JSON.stringify({ email, displayName, photoURL })
+        );
+      } else {
+        setCurrentUser(false);
+        sessionStorage.clear();
+      }
+    });
+  };
+
+  const signUpProvider = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        router.push("/profile");
+        toastSuccessNotify("Logged in successfully!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const forgotPassword = (email) => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        toastWarnNotify("Please check your mail box!");
+      })
+      .catch((err) => {
+        toastErrorNotify(err.message);
+      });
+  };
+  const values = {
+    currentUser,
+    createUser,
+    signIn,
+    logOut,
+    signUpProvider,
+    forgotPassword,
+  };
+  return (
+    <AuthContext.Provider value={values}>{children} </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
